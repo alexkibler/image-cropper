50bab1b60d1e8da1eaba6911315adb808db0d863
Alex Kibler <akibler@fivestardev.com>
2018-07-30 14:04:22 -0400

    added lollipop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,43 +6,26 @@ import { Component, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Angular 6';
-  logs = [];
-  file: File = null;
-  clickedBox: number;
-  imageChangedEvent: any = '';
-  croppedImage: any = '';
-  cropperReady = false;
+  imageEditor: any;
 
-  public config = {
-    ImageName: 'Some image',
-    AspectRatios: ["1:1"],
-    File: null,
-    ImageType: 'image/jpeg'
+  constructor() {
+    this.imageEditor = (<any>window).Lollipop.setOptions({
+      path: "lollipop",
+      appendTo: "body",
+      gallery_icon: true,
+      camera_icon: true,
+      save_icon: true,
+      share_icon: true,
+      onSave: function (data, extension) {
+        console.log(data);
+      }
+    });
   }
-
   fileChange(event) {
 
-    this.imageChangedEvent = event;
   }
   public uploadImage(id: number) {
-    let element: HTMLElement = document.getElementById('file') as HTMLElement;
-    this.clickedBox = id;
-    element.click();
+    this.imageEditor.open();
   }
 
-  imageLoaded() {
-    this.cropperReady = true;
-  }
-
-  public close() {
-    // Fired when the editor is closed.
-  }
-
-
-  imageCroppedBase64(image: string) {
-    this.croppedImage = image;
-    let element: HTMLElement = document.getElementById('image' + this.clickedBox) as HTMLElement;
-    element.setAttribute('style', 'content:url(' + image + ')');
-  }
 }
