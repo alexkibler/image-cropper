d7ae238290fdbdd2a2d6b3c2f93fdd596d8798f0
Alex Kibler <akibler@fivestardev.com>
2018-07-30 11:31:13 -0400

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = 'Angular 6';
+  logs = [];
+  file: File = null;
+  clickedBox: number;
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  cropperReady = false;
+
+  public config = {
+    ImageName: 'Some image',
+    AspectRatios: ["1:1"],
+    File: null,
+    ImageType: 'image/jpeg'
+  }
+
+  fileChange(event) {
+
+    this.imageChangedEvent = event;
+  }
+  public uploadImage(id: number) {
+    let element: HTMLElement = document.getElementById('file') as HTMLElement;
+    this.clickedBox = id;
+    element.click();
+  }
+
+  imageLoaded() {
+    this.cropperReady = true;
+  }
+
+  public close() {
+    // Fired when the editor is closed.
+  }
+
+
+  imageCroppedBase64(image: string) {
+    this.croppedImage = image;
+    let element: HTMLElement = document.getElementById('image' + this.clickedBox) as HTMLElement;
+    element.setAttribute('style', 'content:url(' + image + ')');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { 
+  MatButtonModule,
+  MatCheckboxModule
+} from '@angular/material';
 import { NgModule } from '@angular/core';
+import { ImageCropperModule } from 'ngx-image-cropper';
+
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +17,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    ImageCropperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
